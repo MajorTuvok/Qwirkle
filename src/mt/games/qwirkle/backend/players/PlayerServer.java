9fad9853c66e4231c3e662767bf469680ff676cc
Major Tuvok <kevin.iselborn@t-online.de>
2018-07-31 15:53:31 +0200

    Create Classes

---
+++ src/mt/games/qwirkle/backend/players/PlayerServer.java
@@ -0,0 +1,4 @@
+package mt.games.qwirkle.backend.players;
+
+public class PlayerServer {
+}
--- src/mt/games/qwirkle/gui/StartFrame.java
+++ src/mt/games/qwirkle/gui/StartFrame.java
@@ -1,7 +1,13 @@
 package mt.games.qwirkle.gui;
 
+import mt.games.qwirkle.helper.Constants;
+
 import javax.swing.*;
 
 public class StartFrame extends JFrame {
-
+    public StartFrame() {
+        super("Choose Game-Mode");
+        setSize(Constants.DEFAULT_FRAME_WIDTH, Constants.DEFAULT_FRAME_HEIGHT);
+        setContentPane(rootPane);
+    }
 }
