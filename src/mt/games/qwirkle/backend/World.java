1e8553181cc84a280488fbe3ac694251d04dba83
Major Tuvok <kevin.iselborn@t-online.de>
2018-08-03 16:48:51 +0200

    Update

--- src/mt/games/qwirkle/backend/World.java
+++ src/mt/games/qwirkle/backend/World.java
@@ -1,29 +1,7 @@
 package mt.games.qwirkle.backend;
 
-import mt.games.qwirkle.backend.obstacles.GamePiece;
-import mt.games.qwirkle.backend.obstacles.GamePos;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class World {
-    private List<List<GamePiece>> mGrid;
 
-    public World() {
-        mGrid = new ArrayList<>();
-    }
-
-    public void setPiece(GamePiece piece) {
-        GamePos pos = piece.getPos();
-        assert pos.getX() > 0 && pos.getY() > 0 : "Positions can never be negative!";
-        if (pos.getY() < getGrid().size()) {
-            List<GamePiece> row = getGrid().get(pos.getY());
-        } else {
-
-        }
-    }
-
-    protected List<List<GamePiece>> getGrid() {
-        return mGrid;
-    }
 }
--- src/mt/games/qwirkle/network/server/ServerPipeCreator.java
+++ src/mt/games/qwirkle/network/server/ServerPipeCreator.java
@@ -1,4 +1,16 @@
 package mt.games.qwirkle.network.server;
 
-public class ServerPipeCreator {
+import mt.games.qwirkle.network.StreamConfig;
+
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.util.function.Supplier;
+
+public enum ServerPipeCreator implements Supplier<StreamConfig> {
+    STREAM_CONFIG_SUPPLIER;
+
+    @Override
+    public StreamConfig get() {
+        return new StreamConfig(new PipedInputStream(), new PipedOutputStream());
+    }
 }
